1ba82861ce0eb30f1d090d78f1e54840a2048cbd
Alexander Diaz <alexanderjoemc@gmail.com>
2025-02-04 19:55:36 -0500

    start adding rotation tests

---
+++ src/test/java/dev/alexanderdiaz/variorum/map/rotation/RotationTest.java
@@ -0,0 +1,34 @@
+package dev.alexanderdiaz.variorum.map.rotation;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import dev.alexanderdiaz.variorum.match.Match;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class RotationTest {
+    @Test
+    void testThatRotationIsEmptyAndGetMatchThrows() {
+        // given
+        Rotation rotation = new Rotation(List.of());
+        // when
+        assertTrue(rotation.getMapQueue().isEmpty());
+        // then
+        assertThrows(IllegalStateException.class, rotation::getMatch);
+    }
+
+    @Test
+    void testThatRotationWithAMatchReturnsMatch() {
+        // given
+        Match matchMock = mock(Match.class);
+        Rotation rotation = new Rotation(List.of(matchMock));
+
+        // when
+        rotation.start();
+
+        // then
+        assertEquals(0, rotation.getCurrentIndex().intValue());
+        assertEquals(matchMock, rotation.getMatch());
+    }
+}
